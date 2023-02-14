5bf4319f53824f2155a7a7f4f3f3e028fd26675c
Oleh Pshyk <opshyk@apple.com>
2023-02-14 15:07:18 +0200

    start

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import sexyGuys from './sexyGuys';
+
+document.addEventListener('DOMContentLoaded', () => {
+  sexyGuys();
+
+});
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ src/js/sexyGuys.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+const sexyGuys = () => {
+  console.log('SexyGuys:');
+  const guys = [
+    {
+      name: 'Іванко',
+      age: 14,
+    },
+    {
+      name: 'Василько',
+      age: 18,
+    },
+    {
+      name: 'Алекс',
+      age: 25,
+    },
+    {
+      name: 'Олежик',
+      age: 33,
+    },
+    {
+      name: 'Андрійко',
+      age: 55,
+    },
+  ];
+  console.log(guys);
+
+  //  код пишемо ось тут, він виконається коли сторінка буде загружена повністю
+};
+
+export default sexyGuys;
